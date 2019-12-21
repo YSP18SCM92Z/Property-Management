5777e16856407177c0a2c6581cc969690d2baa05
Tanh Tran Minh Tin Pham <tpham21094@gmail.com>
2019-12-21 13:51:31 -0600

    WIP - 04: Added custom toolbar

--- app/src/main/java/com/nijhoomt/ntrental/MainActivity.kt
+++ app/src/main/java/com/nijhoomt/ntrental/MainActivity.kt
@@ -3,10 +3,12 @@ package com.nijhoomt.ntrental
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        setUpToolbar()
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,7 +28,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_tenants
             )
         )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun setUpToolbar() {
+        val customToolbar = custom_toolbar as Toolbar
+        setSupportActionBar(customToolbar)
     }
 }
