7666fa232852401b3984dc07322b05a02e0a07ec
nijhoom <nijhoomroy@hotmail.com>
2019-12-22 22:51:53 -0600

    Login validated

--- app/src/main/java/com/nijhoomt/ntrental/login/LoginActivity.kt
+++ app/src/main/java/com/nijhoomt/ntrental/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.nijhoomt.ntrental.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -9,6 +11,7 @@ import com.nijhoomt.ntrental.MainActivity
 import com.nijhoomt.ntrental.R
 import com.nijhoomt.ntrental.model.LoginCredential
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,29 +21,51 @@ class LoginActivity : AppCompatActivity() {
 
         btn_login_login.setOnClickListener {
 
-            // Assume you can get the data from edittext for email & password
-            val email = edit_text_email_login.text.toString()
-            val password = edit_text_password_login.text.toString()
+            val email = edit_text_email_login
+            val password = edit_text_password_login
 
-            val loginCredential = LoginCredential(email, password)
+            if(edit_text_email_login.text.toString() == "" || edit_text_password_login.text.toString() == ""){
+                if(edit_text_email_login.text.toString() == ""){
 
-            val loginViewModelFactory = LoginViewModelFactory(
-                loginCredential = loginCredential,
-                application = application
-            )
+                    email.error = "Email field is required"
+                }
 
-            val loginViewModel =
-                ViewModelProviders
-                    .of(this, loginViewModelFactory)
-                    .get(LoginViewModel::class.java)
+                if(edit_text_password_login.text.toString() == ""){
 
-            loginViewModel.loginObject.observe(this, Observer {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("LoginObject", it)
-                startActivity(intent)
-            })
+                    password.error = "Password field is required"
+                }
+            }
+
+           else {
+                processLogin()
+            }
         }
 
 
+    }
+
+    private fun processLogin() {
+        // Assume you can get the data from edittext for email & password
+        val email = edit_text_email_login.text.toString()
+        val password = edit_text_password_login.text.toString()
+
+        val loginCredential = LoginCredential(email, password)
+
+        val loginViewModelFactory = LoginViewModelFactory(
+            loginCredential = loginCredential,
+            application = application
+        )
+
+        val loginViewModel =
+            ViewModelProviders
+                .of(this, loginViewModelFactory)
+                .get(LoginViewModel::class.java)
+
+        loginViewModel.loginObject.observe(this, Observer {
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("LoginObject", it)
+            startActivity(intent)
+        })
     }
 }
