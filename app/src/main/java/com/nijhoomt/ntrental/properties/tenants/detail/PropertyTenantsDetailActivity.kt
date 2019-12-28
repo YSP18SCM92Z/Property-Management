1d641a171bcbef4e2e7e3357e4ce5206c6c758da
Tanh Tran Minh Tin Pham <tpham21094@gmail.com>
2019-12-28 15:50:23 -0600

    Small fix

--- app/src/main/java/com/nijhoomt/ntrental/properties/tenants/detail/PropertyTenantsDetailActivity.kt
+++ app/src/main/java/com/nijhoomt/ntrental/properties/tenants/detail/PropertyTenantsDetailActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.nijhoomt.ntrental.R
+import com.nijhoomt.ntrental.model.Tenant
 
 import kotlinx.android.synthetic.main.activity_property_tenants_detail.*
 
 class PropertyTenantsDetailActivity : AppCompatActivity() {
 
+    private lateinit var chosenTenant: Tenant
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_tenants_detail)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(custom_toolbar_property_tenant_detail)
+
+//        chosenProperty = intent.getSerializableExtra("SELECTED_PROPERTY") as Property
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
